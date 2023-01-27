259d6b9c9b956eae188308082e0ddd0ea45a1d56
Ibrahim.H <bitsnaps@yahoo.fr>
2023-01-27 19:20:39 +0100

    update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 // If you have enabled Take Over Mode or installed the Volar, you can disable generating the shim for *.vue files:
